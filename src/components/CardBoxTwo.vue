357b01048aab646c468f335d728cd79ef0aeadc2
Dimcho Gospodinov <evil112@abv.bg>
2024-05-23 11:17:26 +0300

    update 04

--- src/components/CardBoxTwo.vue
+++ src/components/CardBoxTwo.vue
@@ -8,48 +8,50 @@
                 <span class="text-3xl font-bold">HOW IT WORKS</span>
             </div>
 
-            <div>
-                <div class="flex justify-center space-x-10 max-lg:flex-wrap max-lg:space-x-0 max-lg:space-y-10">
-                    <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl">
-                        <img class="w-16 h-16 mx-auto mb-3" src="@/assets/neuron.png" alt="">
-                        <h2 class="font-bold text-center text-2xl">Enter Your Detail</h2>
-                        <p class="text-[#AEAEAE] text-center mt-3">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore
-                            et dolore magna aliqua. Ut enim ad minim veniam.
-                        </p>
-                    </div>
-                    <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl">
-                        <img class="w-16 h-16 mx-auto mb-3" src="@/assets/circuit.png" alt="">
-                        <h2 class="font-bold text-center text-2xl">Import Data For</h2>
-                        <p class="text-[#AEAEAE] text-center mt-3">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore
-                            et dolore magna aliqua. Ut enim ad minim veniam.
-                        </p>
-                    </div>
-                    <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl">
-                        <img class="w-16 h-16 mx-auto mb-3" src="@/assets/human-brain.png" alt="">
-                        <h2 class="font-bold text-center text-2xl">Create Account</h2>
-                        <p class="text-[#AEAEAE] text-center mt-3">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore
-                            et dolore magna aliqua. Ut enim ad minim veniam.
-                        </p>
-                    </div>
-                    <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl">
-                        <img class="w-16 h-16 mx-auto mb-3" src="@/assets/brain.png" alt="">
-                        <h2 class="font-bold text-center text-2xl">Play For Handover</h2>
-                        <p class="text-[#AEAEAE] text-center mt-3">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore
-                            et dolore magna aliqua. Ut enim ad minim veniam.
-                        </p>
-                    </div>
+            <div class="flex justify-center space-x-10 max-lg:flex-wrap max-lg:space-x-0 max-lg:space-y-10">
+                <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl ">
+                    <img class="w-16 h-16 mx-auto mb-3" src="@/assets/neuron.png" alt="">
+                    <h2 class="font-bold text-center text-2xl">Enter Your Detail</h2>
+                    <p class="text-[#AEAEAE] text-center mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
                 </div>
-                <div class="flex justify-center">
-                    <button class="bg-[#19df7f] text-white font-bold px-8 py-3 rounded-full mt-10">VIEW MORE</button>
+                <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl ">
+                    <img class="w-16 h-16 mx-auto mb-3" src="@/assets/circuit.png" alt="">
+                    <h2 class="font-bold text-center text-2xl">Import Data For</h2>
+                    <p class="text-[#AEAEAE] text-center mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
                 </div>
+                <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl ">
+                    <img class="w-16 h-16 mx-auto mb-3" src="@/assets/human-brain.png" alt="">
+                    <h2 class="font-bold text-center text-2xl">Create Account</h2>
+                    <p class="text-[#AEAEAE] text-center mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
+                </div>
+                <div class="max-w-xs bg-white shadow-lg py-16 rounded-3xl ">
+                    <img class="w-16 h-16 mx-auto mb-3" src="@/assets/brain.png" alt="">
+                    <h2 class="font-bold text-center text-2xl">Play For Handover</h2>
+                    <p class="text-[#AEAEAE] text-center mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <button class="bg-[#19df7f] text-white font-bold px-8 py-3 rounded-full mt-10">VIEW MORE</button>
             </div>
         </div>
     </div>
