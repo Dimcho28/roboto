ebd0b63edd03773fe5ffab54b6dca5cb94de9ab2
Dimcho Gospodinov <evil112@abv.bg>
2024-05-26 22:30:35 +0300

    fix refresh

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -32,6 +32,13 @@ const routes = [
 const router = createRouter({
     routes,
     history: createWebHistory(),
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { left: 0, top: 0 }
+        }
+    }
 });
 
 export default router;
